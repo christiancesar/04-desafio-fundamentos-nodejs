3caf46baca3dab473fe3007eda97fcd0a8801dad
marshmellow <37115113+cellfantasma@users.noreply.github.com>
2020-10-29 06:48:11 -0400

    innitial commit

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,23 +6,56 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransaction {
+  title: string;
+
+  value: number;
+
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
+
+  private balance: Balance;
 
   constructor() {
     this.transactions = [];
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce((accumulator, currentValue) => {
+      if (currentValue.type === 'income') {
+        accumulator += currentValue.value;
+      }
+      return accumulator;
+    });
+
+    const outcome = this.transactions.reduce((accumulator, currentValue) => {
+      if (currentValue.type === 'outcome') {
+        accumulator += currentValue.value;
+      }
+      return accumulator;
+    });
+
+    const total = income - outcome;
+
+    this.balance = {
+      income,
+      outcome,
+      total,
+    };
+    return this.balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransaction): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
